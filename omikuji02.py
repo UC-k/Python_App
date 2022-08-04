a29fef5dab49726881b363b6f650987bb3ec44b3
Napoli_uc <70364759+UC-k@users.noreply.github.com>
2022-08-04 13:44:18 +0900

    Add files via upload

--- omikuji02.py
+++ omikuji02.py
@@ -4,25 +4,19 @@ import random
 def click():
     label["text"] = random.choice(["大吉", "吉", "中吉", "小吉", "末吉", "凶"])
     label.update()
-
 # ------------------------------------------------------------------------------
 root = tk.Tk()
 root.title("おみくじアプリ")
 root.geometry("500x500+400+100")
 root.resizable(False, False)
 
-cvs = tk.Canvas(root, width=500, height=500, bg="#323232")
-cvs.pack()
-
 label = tk.Label(root, text="おみくじ", font=("Helvetica",100),
                 fg="skyblue", bg=None)
 label.place(x=250, y=180, anchor="c")
-
 
 btn = tk.Button(root, text="引く",font=("Helvetica",30),
                 fg="#555555", bg="#ffffff", borderwidth=0,
                 cursor="pointinghand", command=click)
 btn.place(x=250, y=350, anchor="c")
-
 
 root.mainloop()
